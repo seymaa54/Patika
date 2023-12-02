66fc1c03d06f5c3d51529eac53f3ec609fa7efd7
Seyma Nalbant <seymanalbant785@gmail.com>
2023-12-02 21:22:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int mat,fizik,kimya,tarih,muzik;
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("mat not:?");
+        mat=input.nextInt();
+        fizik=input.nextInt();
+        kimya=input.nextInt();
+        tarih=input.nextInt();
+        muzik=input.nextInt();
+
+        int toplam=mat+fizik+kimya+tarih+muzik;
+        double sonuc=toplam/5;
+        System.out.println("ortalamanız:"+sonuc);
+        System.out.println(sonuc>50  ? "geçti":"kaldı");
+
+    }
+}
